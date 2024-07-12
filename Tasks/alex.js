e7e1cf107a5381d26995d44350e1d070b2ea8368
JPSurya-57 <162606746+JPSurya-57@users.noreply.github.com>
2024-07-12 21:05:27 +0530

    Delete Tasks directory

--- Tasks/alex.js
+++
@@ -1,6 +0,0 @@
-function add(){
-    var a =( document.getElementById("val1").value);
-    var b =( document.getElementById("val2").value);
-    // console.log(a+b);
-    document.getElementById("txt").innerHTML=a+b;
-}
--- Tasks/bmi.js
+++
@@ -1,7 +0,0 @@
-function bmi(){
-    var x=parseInt(document.getElementById("weight").value)
-    var y=parseInt(document.getElementById("height").value)
-    var y=y/100
-    var z=x/(y*y)
-    document.getElementById("result").innerText=z.toFixed(2)
-}
